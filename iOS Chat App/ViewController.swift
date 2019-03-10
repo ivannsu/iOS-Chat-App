e2117863bf266138efabb072be681d76e26818cf
ivannsu <ivansu.webmail@gmail.com>
2019-03-10 21:14:42 +0700

    initial xcode project

---
+++ iOS Chat App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOS Chat App
+//
+//  Created by Ivan Su on 3/10/19.
+//  Copyright © 2019 Ivan Su. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
