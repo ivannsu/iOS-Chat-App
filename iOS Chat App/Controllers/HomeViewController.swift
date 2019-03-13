4fa524e4a895e22e45ebe83ecfdf1dbbb8f6530d
ivannsu <ivansu.webmail@gmail.com>
2019-03-13 19:05:10 +0700

    added HomeViewController and link it to storyboard

---
+++ iOS Chat App/Controllers/HomeViewController.swift
@@ -0,0 +1,17 @@
+//
+//  HomeViewController.swift
+//  iOS Chat App
+//
+//  Created by Ivan Su on 3/13/19.
+//  Copyright © 2019 Ivan Su. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
