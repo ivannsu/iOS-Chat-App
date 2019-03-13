a94b9185e466b791e96c463318424228188465cb
ivannsu <ivansu.webmail@gmail.com>
2019-03-13 19:03:08 +0700

    added RegisterViewController and add ui textfield and button

---
+++ iOS Chat App/Controllers/RegisterViewController.swift
@@ -0,0 +1,17 @@
+//
+//  RegisterViewController.swift
+//  iOS Chat App
+//
+//  Created by Ivan Su on 3/13/19.
+//  Copyright © 2019 Ivan Su. All rights reserved.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
