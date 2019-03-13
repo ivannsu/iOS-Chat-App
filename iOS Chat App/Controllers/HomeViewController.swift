abac8a2db190b0ee28f67769468d87e61f06be25
ivannsu <ivansu.webmail@gmail.com>
2019-03-13 20:21:36 +0700

    showing navigation bar only in root stack

--- iOS Chat App/Controllers/HomeViewController.swift
+++ iOS Chat App/Controllers/HomeViewController.swift
@@ -9,14 +9,25 @@
 import UIKit
 
 class HomeViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        navigationController?.setNavigationBarHidden(true, animated: animated)
+        super.viewWillAppear(animated)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        navigationController?.setNavigationBarHidden(false, animated: animated)
+        super.viewWillDisappear(animated)
     }
 
     @IBAction func loginButtonPressed(_ sender: UIButton) {
         performSegue(withIdentifier: "goToLoginView", sender: self)
     }
+    
     @IBAction func registerButtonPressed(_ sender: UIButton) {
         performSegue(withIdentifier: "goToRegisterView", sender: self)
     }
