7aa024571b25d90a4d48f5f0d01875aed2f29d5b
ivannsu <ivansu.webmail@gmail.com>
2019-03-14 22:16:56 +0700

    created custom tableview cell: MessageCell

---
+++ iOS Chat App/Views/MessageCell/MessageCell.swift
@@ -0,0 +1,21 @@
+//
+//  MessageCell.swift
+//  iOS Chat App
+//
+//  Created by Ivan Su on 3/14/19.
+//  Copyright © 2019 Ivan Su. All rights reserved.
+//
+
+import UIKit
+
+class MessageCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+    
+}
