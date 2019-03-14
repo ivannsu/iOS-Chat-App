8577c43e49373bd0abce3441db2608440d46c1c3
ivannsu <ivansu.webmail@gmail.com>
2019-03-14 11:30:33 +0700

    link UIAction to LoginViewController

--- iOS Chat App/Controllers/LoginViewController.swift
+++ iOS Chat App/Controllers/LoginViewController.swift
@@ -10,10 +10,14 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
-
+    @IBAction func loginButtonPressed(_ sender: UIButton) {
+    }
+    
 }
 
