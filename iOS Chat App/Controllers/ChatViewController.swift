1208dd8a7a229fcb2c2ce273b6a075fe4bea04b7
ivannsu <ivansu.webmail@gmail.com>
2019-03-14 11:25:38 +0700

    added ChatView Controller and link it to storyboard

---
+++ iOS Chat App/Controllers/ChatViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ChatViewController.swift
+//  iOS Chat App
+//
+//  Created by Ivan Su on 3/14/19.
+//  Copyright © 2019 Ivan Su. All rights reserved.
+//
+
+import UIKit
+
+class ChatViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
