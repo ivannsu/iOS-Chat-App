1f0ccebf42933acc67a09506d90b9fd201cffa46
ivannsu <ivansu.webmail@gmail.com>
2019-03-14 17:29:55 +0700

    added UITableView and conform the protocol to ChatView

--- iOS Chat App/Controllers/ChatViewController.swift
+++ iOS Chat App/Controllers/ChatViewController.swift
@@ -10,10 +10,32 @@ import UIKit
 
 class ChatViewController: UIViewController {
 
+    let tableView = UITableView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         navigationItem.setHidesBackButton(true, animated: false)
+        
+        tableView.delegate = self
     }
 
+}
+
+extension ChatViewController: UITableViewDelegate {
+    
+}
+
+extension ChatViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // let cell = tableView.dequeueReusableCell(withIdentifier: "chatCell", for: indexPath)
+        let cell = UITableViewCell(style: .default, reuseIdentifier: nil)
+        
+        cell.textLabel?.text = "Hello world"
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
 }
